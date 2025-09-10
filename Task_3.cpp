336d16bc2a93d9da2890c5b1e8975da65a87a940
Ivan_Khramoy <ivankhramoi9@gmail.com>
2025-09-10 09:51:42 +0300

    updated error handlers 2.0

--- Task_3.cpp
+++ Task_3.cpp
@@ -10,11 +10,32 @@ int main() {
 	cout << "¬ведите начальное значение x: " << endl;
 	cin >> start;
 
+  if (!cin) {
+    cerr << "Invalid input: enyer the number" << endl;
+    return 1;
+  }
+ 
 	cout << "¬ведите конечное значение x: ";
 	cin >> end;
+  if (!cin) {
+    cerr << "Invalid input: enyer the number" << endl;
+    return 1;
+  }
+  if (end < 0) {
+    cerr << "Invalid input: cannot be negative" << endl;
+    return 1;
+  }
 
 	cout << "¬ведите шаг: ";
 	cin >> step;
+  if (!cin) {
+    cerr << "Invalid input: enyer the number" << endl;
+    return 1;
+  }
+  if (step < 0) {
+    cerr << "Invalid input: cannot be negative" << endl;
+    return 1;
+  }
 
 	for (double i = start; i<= end; i += step) {
 		double y = sin(i);
