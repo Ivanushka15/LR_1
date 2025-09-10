5566eae6b0c444437aa18f65eaf5e56542b7bac8
Ivan_Khramoy <ivankhramoi9@gmail.com>
2025-09-10 06:33:46 +0300

    updated error handlers

--- Task_3.cpp
+++ Task_3.cpp
@@ -2,16 +2,40 @@
 #include <math.h>
 
 int main() {
+setlocale(LC_ALL, "RU");
 double start, end, step;
-std::cout << "Введите начальное значение x: ";
+
+std::cout << "Enter the initial value x: ";
 std::cin >> start; // Задаем исходное значение переменной
-std::cout << "Введите конечное значение x: ";
-std::cin >> end; // Задаем конечно значение переменной
-std::cout << "Введите шаг: ";
+if (!std::cin) {
+  std::cerr<<"Invalid input: enyer the number" << std::endl;
+  return 1;
+}
+
+std::cout << "Enter the final value x: ";
+std::cin >> end; // Задаем конечноe значение переменной
+if (!std::cin) {
+	std::cerr << "Invalid input: enyer the number" << std::endl;
+	return 1;
+}
+if (end < 0) {
+  std::cerr << "Invalid input: can't be negative" << std::endl;
+  return 1;
+}
+
+std::cout << "Enter the step: ";
 std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
+if (!std::cin) {
+	std::cerr << "Invalid input: enyer the number" << std::endl;
+	return 1;
+}
+if (step < 0) {
+  std::cerr << "Invalid input: cannot be negative" <<std::endl;
+}
 for (double x = start; x <= end; x += step) {
 double y = sin(x); // функция y = sin(x)
 std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
+return 1;
 }
 return 0;
 }
